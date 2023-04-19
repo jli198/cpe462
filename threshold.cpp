686852e4df95cff8777e6a675acde0136fdffbc7
jli198 <jli198@stevens.edu>
2023-04-18 23:10:59 -0400

    threshold

--- threshold.cpp
+++ threshold.cpp
@@ -1,6 +1,7 @@
 /**********************************************************************************
 * imageprocessing.c
 * Usage: imageproc in_file_name out_file_name width height
+* Modified to include HW7. Functionality will need tweaking
  **********************************************************************************/
 
 
@@ -104,7 +105,42 @@ int main(int argc, char *argv[])
 /* Image Processing ends                                          */
 /********************************************************************/
 
+  //HW 7 code
+  new_T = 200.0;  // initial threshold
+  delta_T = 100.0;  // any big number to get into loop
 
+  while (delta_T > 5.0 || delta_T < -5.0)
+  {
+    count1 = 0; count2 = 0;
+    sum1 = 0.0; sum2 = 0.0;
+
+    for (j=0; j<height; j++)
+      for (k=0; k<width; k++)
+      {
+        if (image_in[j][k] >= new_T)
+        {
+          sum1 += image_in[j][k];
+          count1++;
+        }
+        else
+        {
+          sum2 += image_in[j][k];
+          count2++;
+        }
+      }
+    
+    old_T = new_T;
+    new_T = ((sum1/count1) + (sum2/count2)) / 2;
+    delta_T = new_T - old_T;
+    printf("new threshold = %f \n", new_T);
+  }
+
+  // now do thresholding
+  for (j=0; j<height; j++)
+    for (k=0; k<width; k++)
+    {
+      image_out[j][k] = (image_in[j][k] >= new_T ? 255 : 0);
+    }
 
 	/* save image_out into out_file in RAW format */
 	for (j=0; j<height; j++)
